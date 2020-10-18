80c45ec3520b732292781c1c27cfe28a8e372dfd
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-18 15:41:37 +0800

    改造input框，添加清除按钮

--- src/components/input/Input.vue
+++ src/components/input/Input.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="wrapper">
-    <label>
-      <input v-bind="$attrs" :class="{'error': error}" :value="value" :disabled="disabled" :readonly="readonly"
+    <div class="input-wrapper">
+      <input ref="input" v-bind="$attrs" :class="{'error': error}" :value="value" :disabled="disabled"
+             :readonly="readonly"
              @change="$emit('change', $event.target.value)"
              @input="$emit('input', $event.target.value)"
              @focus="$emit('focus', $event.target.value)"
              @blur="$emit('blur', $event.target.value)"
       />
-    </label>
+      <div class="clear" v-if="clear" @click="clearValue">x</div>
+    </div>
+
     <template v-if="error">
       <g-icon icon="error" class="error-icon"></g-icon>
       <span class="error-message">{{error}}</span>
@@ -17,7 +20,7 @@
 
 <script>
   export default {
-    inheritAttrs:false,
+    inheritAttrs: false,
     props: {
       value: {
         type: String,
@@ -32,8 +35,21 @@
       },
       error: {
         type: String
+      },
+      clear: {
+        type: Boolean,
+        default: true
       }
     },
+    mounted() {
+      console.log(this.$refs.input.value);
+    },
+    methods: {
+      clearValue() {
+        if (!this.$refs.input.value){return}
+        this.$refs.input.value = '';
+      }
+    }
   };
 </script>
 
@@ -46,38 +62,55 @@
     justify-content: center;
     align-items: center;
 
-    :not(:last-child){
+    :not(:last-child) {
       margin-right: .8em;
     }
 
+    .input-wrapper {
+      position: relative;
 
-    input {
-      height: $button-height;
-      border: 1px solid  #999999;
-      border-radius: $border-radius;
-      padding: 0 8px;
-      font-size: inherit;
 
-      &:hover {
-        border-color: #666;
+      input {
+        height: $button-height;
+        border: 1px solid #999999;
+        border-radius: $border-radius;
+        padding: 0 8px;
+        font-size: inherit;
+
+        &:hover {
+          border-color: #666;
+        }
+
+        &:focus {
+          outline: none;
+          box-shadow: 0 0 0 2px #99caff
+        }
+
+        &[disabled], &[readonly] {
+          border-color: #bbb;
+          color: #bbb;
+          cursor: not-allowed;
+          user-select: none;
+        }
+
+        &.error {
+          border-color: red;
+        }
       }
 
-      &:focus {
-        outline: none;
-        box-shadow: 0 0 0 2px #99caff
-      }
+      .clear {
+        position: absolute;
+        top: 0;
+        right: 1em;
+        width: 2em;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-      &[disabled], &[readonly] {
-        border-color: #bbb;
-        color: #bbb;
-        cursor: not-allowed;
-        user-select:none;
-      }
-
-      &.error {
-        border-color: red;
       }
     }
+
 
     .error-icon {
       fill: red;
