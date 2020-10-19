75cd05f17e3c7c6f71cf38c192b57d3086c4c491
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-20 06:00:07 +0800

    样式要加在index.html里面

--- App.vue
+++ App.vue
@@ -14,4 +14,21 @@
 <style lang="scss" scoped>
   @import "src/assets/reset.scss";
   @import "src/assets/helper.scss";
+  html {
+    --button-bg: white;
+    --button-active-bg: #eee;
+    --color: #333;
+    --border-color: #999;
+    --border-color-hover: #666;
+    --box-shadow-color: rgb(0, 0, 0, 0.5)
+  }
+
+  #app {
+    margin: 20px
+  }
+
+  body {
+    height: 100vh;
+    font-size: $font-size;
+  }
 </style>
